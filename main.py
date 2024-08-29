dda3970217c7db63a94c52c5dcf87fce37da4dad
Pedroprog2 <45775554+Pedroprog2@users.noreply.github.com>
2024-08-29 10:17:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ def process_image(image):
 
 # Subtítulo para a seção de upload
 st.title('Análise de pH do solo por meio de imagens digitais e repolho roxo')
+st.write('Atualizado hoje (29/08/2024)')
 st.write('Bem-vind@!')
 
 #st.subheader('Orientações')
